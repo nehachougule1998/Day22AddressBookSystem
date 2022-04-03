ff29df49741b81675d359de74a5c65d8177f42b7
nehachougule1998 <nchougule734@gmail.com>
2022-04-03 16:03:05 +0530

    Added

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -5,9 +5,12 @@ import java.util.ArrayList;
 
 public class AddressBook {
 
-	static ArrayList<Contact> list = new ArrayList<Contact>();
+	ArrayList<Contact> list = new ArrayList<Contact>();
+	String bookName; /// It represent the name of book
 
-	void addContact(Contact contact) {
+	void addContact() {
+		Contact contact = new Contact();
+		contact.addContact();
 		list.add(contact);
 		System.out.println("Contact added successfully");
 	}
@@ -33,14 +36,13 @@ public class AddressBook {
 	}
 
 	void editPerson(String name, ArrayList<Contact> list) {
-		if(list.size()==0){
+		if (list.size() == 0) {
 			System.out.println("Addressbook is empty.Please add First");
-		}
-		else {
+		} else {
 			int m = 0;
 			for (int i = list.size() - 1; i >= 0; --i) {
 				if (list.get(i).firstName.contains(name)) {
-					list.get(i).addInfo();
+					list.get(i).addContact();
 					System.out.println("Contact Updated successfully");
 					m += 1;
 					break;
@@ -50,43 +52,64 @@ public class AddressBook {
 				System.out.println("No contact with the given name exist");
 			}
 		}
-		
+
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
+
 		System.out.println("Welcome to Address Book Program ");
-		AddressBook addressbook = new AddressBook();
-		Contact contact = new Contact();
+
+		BookList shelf = new BookList();
 
 		while (true) {
-			System.out.println("Do you want to add/edit/delete the contact (0/1/2) :Press 3 to quit");
-			int input = scan.nextInt();
-			if (input == 0) {
-				contact.addInfo();
-				addressbook.addContact(contact);
-
-			} else if (input == 1) {
-				Scanner scan1=new Scanner(System.in);
-				System.out.println("Enter the first name of person you to edit ");
-				String name = scan1.nextLine();
-				addressbook.editPerson(name, list);
-
-			} else if (input == 2) {
-				Scanner scan2=new Scanner(System.in);
-				System.out.println("Enter the first name of the person you want to delete : ");
-				String name = scan2.nextLine();
-				addressbook.deletePerson(name, list);
-			}
-
-			else if (input == 3) {
-				System.out.println("Program successfully closed");
+			AddressBook addressBook = new AddressBook();
+			Scanner scan3 = new Scanner(System.in);
+			System.out.println("Enter the name of Book you want to  access or add or press 'q' to quit");
+			String bookName = scan3.nextLine();
+			if (bookName.equals("q")) {
+				//if (addressBook.list.size() > 0) {
+					//book.addBook(bookName, addressBook);
+				//}
+				System.out.println("The program is closed");
 				break;
 			}
+			int result = shelf.checkBook(bookName);//// (It can return 0 or 1)It will return 1 if book exist b and break
+													//// down loop
+			int condition = 0;///// It will keep check on the addressbook created or not
+			while (true) {
+				if (result == 1) {
+					break;
+				}
+				System.out.println("Do you want to add/edit/delete the contact (0/1/2) :Press 3 to Go back to main menu");
+				Scanner scan = new Scanner(System.in);
+				int input = scan.nextInt();
 
-			else {
-				System.out.println("Enter the valid command");
+				if (input == 0) {
+
+					addressBook.addContact();
+
+				} else if (input == 1) {
+					Scanner scan1 = new Scanner(System.in);
+					System.out.println("Enter the first name of person you to edit ");
+					String name = scan1.nextLine();
+					addressBook.editPerson(name, addressBook.list);
+
+				} else if (input == 2) {
+					Scanner scan2 = new Scanner(System.in);
+					System.out.println("Enter the first name of the person you want to delete : ");
+					String name = scan2.nextLine();
+					addressBook.deletePerson(name, addressBook.list);
+				}
+
+				else if (input == 3) {
+					shelf.addBook(bookName, addressBook);
+					break;
+				}
+
+				else {
+					System.out.println("Enter the valid command");
+				}
 			}
 		}
 
